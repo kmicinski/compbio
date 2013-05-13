441375f8fc4fa26cd770abfcd1da632e6df4fb61
jp.rider63 <dev@jamesparker.me>
2013-05-13 15:36:34 -0400

    Some more IntervalTree work.

--- src/ITree.jl
+++ src/ITree.jl
@@ -3,7 +3,7 @@
 
 module ITree
 
-export Interval, IntervalTree, EmptyNode, IntervalNode, intervalTree
+export Interval, IntervalTree, intervalTree #, EmptyNode, IntervalNode
 
 abstract Interval
 abstract IntervalTree{T <: Interval}
@@ -17,30 +17,68 @@ type IntervalNode{T} <: IntervalTree{T}
 	backwardIntervals::Vector{T}
 	left::IntervalTree{T}
 	right::IntervalTree{T}
+
+	# TODO: Can this be made tail recursive?
+	function IntervalNode( intervals::Vector{T})
+		center = middle( intervals)
+
+		# TODO: can I prealloc a length? Or use a list or something..
+		leftIntervals = Array( Interval, 0)
+		overlapIntervals = Array( Interval, 0)
+		rightIntervals = Array( Interval, 0)
+
+		# Add intervals to their appropriate sets.
+		for i in intervals
+			if i < center
+				push!( leftIntervals, i)
+			else if i > center
+				push!( rightINtervals, i)
+			else
+				push!( overlapIntervals, i)
+			end
+		end
+
+		forwardIntervals = # ... sortby
+
+	end
 end
 
 function intervalTree{T <: Interval}( intervals::Vector{T})
-	tree = EmptyNode()
-	for i in intervals
-		insert!( tree, i)
+	if length( intervals) == 0
+		EmptyNode()
+	else
+		IntervalNode( intervals)
 	end
-	tree
 end
 
-function insert!{T <: Interval}( tree::IntervalTree{T}, interval::T)
-	# TODO ...
+#function insert!{T <: Interval}( tree::IntervalTree{T}, interval::T)
+#	# TODO ...
+#
+#	#empty = EmptyNode()
+#	#node = IntervalNode( interval, empty, empty)
+#	#insert!( tree, node)
+#end
+#
+#function insert!{T <: Interval}( tree::EmptyNode{T}, node::IntervalNode{T})
+#	node
+#end
+#
+#function insert!{T <: Interval}( tree::IntervalNode{T}, node::IntervalNode{T})
+#	# TODO ...
+#end
 
-	#empty = EmptyNode()
-	#node = IntervalNode( interval, empty, empty)
-	#insert!( tree, node)
-end
+# Finds the middle of the intervals.
+# Somewhat naive. Could improve to split more evenly.
+function middle{T <: Interval}( intervals::Vector{T})
+	minn = Inf
+	maxx = -Inf
 
-function insert!{T <: Interval}( tree::EmptyNode{T}, node::IntervalNode{T})
-	node
-end
+	for i in intervals
+		minn = min( minn, i.start)
+		maxx = max( maxx, i.finish)
+	end
 
-function insert!{T <: Interval}( tree::IntervalNode{T}, node::IntervalNode{T})
-	# TODO ...
+	(minn + maxx) / 2.0
 end
 
 end
