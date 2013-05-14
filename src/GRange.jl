24a24dd11dbb57ce098235e74e926f01671563b5
Matthew Mauriello <mattm@cs.umd.edu>
2013-05-14 19:52:42 -0400

    Added Comparer to GRanges
    Added comparers to GRanges. However, not sure what functions on lines
    51, 64, 68 are really comparing because they appear to be object to
    number and object to object... please advise.

--- src/GRange.jl
+++ src/GRange.jl
@@ -9,7 +9,7 @@
 #
 #   See GRanges.jl for notes.
 
-export GRange, start, finish, width, name
+export GRange, start, finish, width, name, isless, isequal, isless, isequal
 
 type GRange
     seqname::String
@@ -42,3 +42,30 @@ end
 function name(gr::GRange)
     return gr.range.name
 end
+
+# Define equality for IRange as equal when the ranges don't overlap at all.
+function isless( r1::GRange, r2::GRange)
+        finish(r1) < start(r2)
+end
+
+function isequal( r1::GRange, r2::GRange)
+        !(r1 < r2 || r2 < r1)
+end
+
+# Define comparisons relative to a point.
+function isless( x::Float64, r::GRange)
+        x < start(r)
+end
+
+function isless( r::GRange, x::Float64)
+        finish(r) < x
+end
+
+function isequal( x::Float64, r::GRange)
+        !(x < r || r < x)
+end
+
+function isequal( r::GRange, x::Float64)
+        isequal( x, r)
+end
+
