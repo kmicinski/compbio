3dd4f38f10d04e0f16142190cf68e4e0eabfe35f
jp.rider63 <dev@jamesparker.me>
2013-05-15 12:33:15 -0400

    Added abstract Interval functions to emulate interfaces.

--- src/IntervalDef.jl
+++ src/IntervalDef.jl
@@ -2,8 +2,15 @@
 
 module IntervalDef
 
-export Interval
+export Interval, start, finish
 
 abstract Interval
+
+function start( i::Interval) # Not sure if these methods actually do anything..
+	error( "`$(typeof(i))` does not implement `start`")
+end
+function finish( i::Interval) # Not sure if these methods actually do anything..
+	error( "`$(typeof(i))` does not implement `finish`")
+end
 
 end
